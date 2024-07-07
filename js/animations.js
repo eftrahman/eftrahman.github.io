32bccd514258020d8b4b48d4c44bc806cf290836
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-07-08 02:38:12 +0600

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,20 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cards = document.querySelectorAll(".card");
-    const options = {
-        threshold: 0.5,
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.style.transform = "scale(1.05)";
-                entry.target.style.transition = "transform 0.5s ease-in-out";
-                observer.unobserve(entry.target);
-            }
-        });
-    }, options);
 
     cards.forEach(card => {
-        observer.observe(card);
+        card.addEventListener("mouseover", function () {
+            this.style.transform = "scale(1.05)";
+            this.style.transition = "transform 0.3s ease-in-out";
+        });
+
+        card.addEventListener("mouseout", function () {
+            this.style.transform = "scale(1)";
+            this.style.transition = "transform 0.3s ease-in-out";
+        });
     });
 });
