21afd1731d8278cf9e4ea9656e2cb1515dadbad0
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-07-09 01:38:35 +0600

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll(".card");
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("header");
+    header.style.animation = "fadeIn 2s ease-in-out";
 
+    const cards = document.querySelectorAll(".card");
     cards.forEach(card => {
+        card.style.animation = "zoomInCard 1s ease-in-out";
+
         card.addEventListener("mouseover", function () {
-            this.style.transform = "scale(1.2)";
+            this.style.transform = "scale(1.1)";
             this.style.transition = "transform 0.3s ease-in-out";
         });
 
